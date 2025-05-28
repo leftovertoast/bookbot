7407750df17729b18fb9c6779d31fd2337c90cb5
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-05-28 15:29:36 +0000

    finished bookbot project

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_number_of_words, count_of_letters, generate_report
 
 def get_book_text(path):
@@ -6,13 +7,19 @@ def get_book_text(path):
         return contents
 
 def main():
-    filepath = "books/frankenstein.txt"
+    try:
+        filepath = sys.argv[1]
+    except IndexError:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     #print(text)
     mynum = get_number_of_words(text)
     #print(f"{mynum} words found in the document")
     lower_text = count_of_letters(text)
     #print(lower_text)
+    
     report = generate_report(lower_text)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
