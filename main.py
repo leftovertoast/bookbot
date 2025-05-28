449abf491aec6b0f714a8eaa3cbf8fc5929a6541
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-05-28 11:51:05 +0000

    notes added to gitignore file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_number_of_words 
+from stats import get_number_of_words, count_of_letters
 
 def get_book_text(path):
     with open(path) as f:
@@ -10,7 +10,8 @@ def main():
     print(text)
     mynum = get_number_of_words(text)
     print(f"{mynum} words found in the document")
-
+    lower_text = count_of_letters(text)
+    print(lower_text)
 
 main()
     
--- stats.py
+++ stats.py
@@ -2,5 +2,40 @@ def get_number_of_words(book_content):
     result = len(book_content.split())
     return result
 
-#def count_of_letters(book_content):
-    
+def count_of_letters(book_content):
+    lower_book = book_content.lower()
+    alpha_dict = {
+        "a": 0,
+        "b": 0,
+        "c": 0,
+        "d": 0,
+        "e": 0,
+        "f": 0,
+        "g": 0,
+        "h": 0,
+        "i": 0,
+        "j": 0,
+        "k": 0,
+        "l": 0,
+        "m": 0,
+        "n": 0,
+        "o": 0,
+        "p": 0,
+        "q": 0,
+        "r": 0,
+        "s": 0,
+        "t": 0,
+        "u": 0,
+        "v": 0,
+        "w": 0,
+        "x": 0,
+        "y": 0,
+        "z": 0
+    }
+    for char in lower_book: 
+        if char in alpha_dict:
+            alpha_dict[char] += 1
+    return alpha_dict
+
+def generate_report(count_dict):
+    return "done"
